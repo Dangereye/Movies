bad44c79469c8d47442aed29f9dd32fcfdd43011
Craig Puxty <craig_puxty@talktalk.net>
2021-07-04 18:14:32 +0100

    Updated copyright info.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer id='footer'>
-      <div className='container'>
-        <p className='copyright'>&copy; Dangereye 2021.</p>
+    <footer id="footer">
+      <div className="container">
+        <p className="copyright">&copy; Craig Puxty 2021.</p>
       </div>
     </footer>
   );
